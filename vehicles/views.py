8aaa0319d53df6bb3cb09fc36bd9cf88ac4b0826
Akash Ashok <akashask2012@gmail.com>
2023-10-05 15:27:00 +0530

    second

--- vehicles/views.py
+++ vehicles/views.py
@@ -2,8 +2,23 @@ from django.shortcuts import render,redirect
 from django.views.generic import View,TemplateView
 from vehicles.forms import *
 from vehicles.models import *
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.
 
 
 class RegistrationView(View):
+    def get(self,request,*args,**kwargs):
+        form=RegistrationForm()
+        return render(request,"registration.html",{"form":form})
     
+    def post(self,request,*args,**kwargs):
+        form=RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Registartion Succesfull")
+            return redirect("login")
+        else:
+            messages.error(request,"Registration failed")
+            return render(request,"registration.html",{"form":form})
+
